35e66df3bc98f2d2ba5e8d08fcb99edb481067b6
Olawande Akinmosin <samueldacoal@gmail.com>
2026-02-20 13:20:30 +0100

    feat: Update landing page components with new feature images and testimonials, enhancing user engagement and visual appeal.

--- app/components/landing/BookADemoForm.tsx
+++ app/components/landing/BookADemoForm.tsx
@@ -21,7 +21,6 @@ const BookADemoForm = ({ children }: { children: React.ReactNode }) => {
 			return;
 		}
 		const token = await executeRecaptcha("demo_form");
-		console.log(token);
 
 		formData.append("reCAPTCHA", token);
 
@@ -51,100 +50,111 @@ const BookADemoForm = ({ children }: { children: React.ReactNode }) => {
 						</div>
 					)}
 
-					<div className="grid sm:grid-cols-2 gap-x-[42.43px] gap-y-[30.55px]">
-						<div className="flex flex-col gap-2">
-							<label
-								className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
-								htmlFor="first_name"
-							>
-								Name <span className="text-accent-green">*</span>
-							</label>
-							<input
-								type="text"
-								id="name"
-								name="name"
-								placeholder="Full name"
-								className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
-								required
-							/>
-							{state?.errors?.name && (
-								<p className="text-red-500 text-xs">{state.errors.name[0]}</p>
-							)}
-						</div>
+					{state?.success ? (
+						""
+					) : (
+						<>
+							<div className="grid sm:grid-cols-2 gap-x-[42.43px] gap-y-[30.55px]">
+								<div className="flex flex-col gap-2">
+									<label
+										className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
+										htmlFor="first_name"
+									>
+										Name <span className="text-accent-green">*</span>
+									</label>
+									<input
+										type="text"
+										id="name"
+										name="name"
+										placeholder="Full name"
+										className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
+										required
+									/>
+									{state?.errors?.name && (
+										<p className="text-red-500 text-xs">
+											{state.errors.name[0]}
+										</p>
+									)}
+								</div>
 
-						<div className="flex flex-col gap-2">
-							<label
-								className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
-								htmlFor="email"
-							>
-								Email Address <span className="text-accent-green">*</span>
-							</label>
-							<input
-								type="email"
-								id="email"
-								name="email"
-								placeholder="example@example.com"
-								className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
-								required
-							/>
-							{state?.errors?.email && (
-								<p className="text-red-500 text-xs">{state.errors.email[0]}</p>
-							)}
-						</div>
+								<div className="flex flex-col gap-2">
+									<label
+										className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
+										htmlFor="email"
+									>
+										Email Address <span className="text-accent-green">*</span>
+									</label>
+									<input
+										type="email"
+										id="email"
+										name="email"
+										placeholder="example@example.com"
+										className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
+										required
+									/>
+									{state?.errors?.email && (
+										<p className="text-red-500 text-xs">
+											{state.errors.email[0]}
+										</p>
+									)}
+								</div>
+							</div>
+							<div className="flex flex-col gap-2 mt-[30.55px]">
+								<label
+									className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
+									htmlFor="community_name"
+								>
+									Residence/Community/Company Name{" "}
+									<span className="text-accent-green">*</span>
+								</label>
+								<input
+									type="text"
+									id="community_name"
+									name="community_name"
+									placeholder="haiven estate"
+									className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
+									required
+								/>
+								{state?.errors?.community_name && (
+									<p className="text-red-500 text-xs">
+										{state.errors.community_name[0]}
+									</p>
+								)}
+							</div>
 
-						<div className="flex flex-col gap-2">
-							<label
-								className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
-								htmlFor="community_name"
-							>
-								Residence/Community/Company Name{" "}
-								<span className="text-accent-green">*</span>
-							</label>
-							<input
-								type="text"
-								id="community_name"
-								name="community_name"
-								placeholder="haiven estate"
-								className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px]  placeholder:text-[#979797] text-black"
-								required
-							/>
-							{state?.errors?.community_name && (
-								<p className="text-red-500 text-xs">
-									{state.errors.community_name[0]}
-								</p>
-							)}
-						</div>
-					</div>
+							<div className="mt-[30.55px] flex flex-col gap-2">
+								<label
+									className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
+									htmlFor="message"
+								>
+									What problem are you trying to solve?{" "}
+									<span className="text-accent-green">*</span>
+								</label>
+								<textarea
+									id="message"
+									name="message"
+									rows={10}
+									placeholder="Leave your message"
+									className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px] placeholder:text-[#979797] text-black"
+									required
+								></textarea>
+								{state?.errors?.message && (
+									<p className="text-red-500 text-xs">
+										{state.errors.message[0]}
+									</p>
+								)}
+							</div>
 
-					<div className="mt-[30.55px] flex flex-col gap-2">
-						<label
-							className="font-regular text-[13.58px] leading-[20.37px] -tracking-[0.25px]"
-							htmlFor="message"
-						>
-							What problem are you trying to solve?{" "}
-							<span className="text-accent-green">*</span>
-						</label>
-						<textarea
-							id="message"
-							name="message"
-							rows={10}
-							placeholder="Leave your message"
-							className="border-[0.85px] border-[#DADADA] rounded-md p-2 bg-white h-auto py-[13.58px] px-[10.54px] font-regular text-[13.58px] leading-[20.37px] placeholder:text-[#979797] text-black"
-							required
-						></textarea>
-						{state?.errors?.message && (
-							<p className="text-red-500 text-xs">{state.errors.message[0]}</p>
-						)}
-					</div>
-
-					<div className="mt-[47.52px]">
-						<Button
-							disabled={isPending}
-							className=" h-auto px-[78.69px] font-medium py-[13.58px] bg-accent-green text-white font-regular leading-[20.37px] rounded-[16.97px] disabled:opacity-50"
-						>
-							{isPending ? "Submitting..." : "Submit Message"}
-						</Button>
-					</div>
+							<div className="mt-[47.52px]">
+								<Button
+									disabled={isPending}
+									className=" h-auto px-[78.69px] font-medium py-[13.58px] bg-accent-green text-white font-regular leading-[20.37px] rounded-[16.97px] disabled:opacity-50"
+								>
+									{isPending ? "Submitting..." : "Submit Message"}
+								</Button>
+							</div>
+						</>
+					)}
 				</form>
 			</DialogContent>
 		</Dialog>
--- app/components/landing/Testimonial.tsx
+++ app/components/landing/Testimonial.tsx
@@ -1,53 +1,74 @@
 "use client";
 
-import React from "react";
-import SectionBadge from "./SectionBadge";
-import Image from "next/image";
-import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import SectionBadge from "./SectionBadge";
 
-const testimonial = [
+export const testimonials = [
 	{
-		id: "1",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 1,
+		name: "Tunde A",
+		community: "Facility Manager - Oba Elegushi",
 		testimony:
-			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+			"Before Haiven, we were using too many tools to manage simple things. Now, I can track maintenance, approve requests, and stay updated in real-time. It just makes my work less stressful.",
+		image: "/images/tunde-a.webp",
 	},
 	{
-		id: "2",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 2,
+		name: "Grace O",
+		community: "Facility Manager - Pearl Nuga Park Estate",
 		testimony:
-			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+			"I don't have to chase residents about bills anymore. Haiven helps me send reminders and keep track of payments without stress. It really makes my job easier.",
+		image: "/images/grace-o.webp",
 	},
 	{
-		id: "3",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 3,
+		name: "Yewande K",
+		community: "Resident - Lekki Garden Phase 1",
 		testimony:
-			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+			"Everything I need is on the app, paying bills, checking my payment history, even sending access codes to guests. It just makes life simpler.",
+		image: "/images/yewande-k.webp",
 	},
 	{
-		id: "4",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 4,
+		name: "Chuka E",
+		community: "Real Estate Developer - PWAN",
 		testimony:
-			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+			"We use Haiven across a few of our estates. It helps us stay organized and gives residents a better experience. It's now part of our standard setup.",
+		image: "/images/chuka-e.webp",
 	},
 	{
-		id: "5",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 5,
+		name: "Chinedu I",
+		community: "Facility Manager - Oba Elegushi",
 		testimony:
-			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+			"Before Haiven, we were using too many tools to manage simple things. Now, I can track maintenance, approve requests, and stay updated in real-time. It just makes my work less stressful.",
+		image: "/images/chinedu-i.webp",
 	},
 	{
-		id: "6",
-		name: "Joshua Biyere",
-		community: "Lekki Phase 1",
+		id: 6,
+		name: "Amaka I",
+		community: "Estate Manager - Cedarwoods Apartments",
 		testimony:
 			"Since we started using Haiven, communication with residents has improved a lot. I can send updates quickly, and the security team now logs every visitor easily. It just works.",
+		image: "/images/amaka-i.webp",
+	},
+	{
+		id: 7,
+		name: "Anonymous",
+		community: "Chief Security Officer - Victory Estate",
+		testimony:
+			"Before Haiven, we relied on handwritten gate logs, people could enter with just a name. Now, with guest codes, we know exactly who's coming in, when and when they exit the estate. It's safer, smarter, and my guards love it.",
+		image: "",
+	},
+	{
+		id: 8,
+		name: "Daniel A",
+		community: "Artisan",
+		testimony:
+			"Joining Haiven's vendor marketplace gave my electrical business more visibility. I now get steady jobs from estates on the platform, and it's helped me grow my income and reputation.",
+		image: "/images/daniel-a.webp",
 	},
 ];
 
@@ -106,27 +127,35 @@ const Testimonial = () => {
 			<div className="mt-[75.47px]">
 				<div className="mb-[56.99px] overflow-hidden" ref={emblaRef}>
 					<div className="flex items-center ">
-						{testimonial.map((item) => (
-							<div key={item.id} className="shrink-0 basis-[380px] px-2">
+						{testimonials.map((item) => (
+							<div
+								key={item.id}
+								className="shrink-0 h-[280px] basis-[380px] px-2"
+							>
 								<TestimonialCard
 									id={item.id}
 									community={item.community}
 									name={item.name}
 									testimony={item.testimony}
+									image={item.image}
 								/>
 							</div>
 						))}
 					</div>
 				</div>
 				<div className="overflow-hidden" ref={rtlEmblaRef}>
-					<div className="flex items-center">
-						{testimonial.map((item) => (
-							<div key={item.id} className="shrink-0 basis-[380px] px-2">
+					<div className="flex">
+						{testimonials.map((item) => (
+							<div
+								key={item.id}
+								className="shrink-0 h-[280px] basis-[380px] px-2"
+							>
 								<TestimonialCard
 									id={item.id}
 									community={item.community}
 									name={item.name}
 									testimony={item.testimony}
+									image={item.image}
 								/>
 							</div>
 						))}
@@ -138,23 +167,25 @@ const Testimonial = () => {
 };
 
 interface Props {
-	id: string;
+	id: number;
 	name: string;
 	community: string;
 	testimony: string;
+	image: string;
 }
 
-function TestimonialCard({ community, id, name, testimony }: Props) {
+function TestimonialCard({ community, id, name, testimony, image }: Props) {
 	return (
-		<div className="w-full">
-			<div className="p-[20.36px] font-sans bg-white   rounded-[10.18px]">
+		<div className="size-full">
+			<div className="p-[20.36px] font-sans bg-white h-full  rounded-[10.18px]">
 				<div className="flex gap-[25.45px] mb-[15.45px]">
 					<div className="size-[55.99px] rounded-full">
 						<Image
-							src="/images/User.png"
+							src={image || "/images/User.png"}
 							alt="user"
 							width={55.99}
 							height={55.99}
+							className="rounded-full"
 						/>
 					</div>
 					<div className="">
@@ -176,5 +207,4 @@ function TestimonialCard({ community, id, name, testimony }: Props) {
 		</div>
 	);
 }
-
 export default Testimonial;
