d8abf2958df271cfa9ee8a56b477ab30028c1469
Olawande Akinmosin <samueldacoal@gmail.com>
2026-03-16 09:34:34 +0100

    adds GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import RecaptchaProvider from "./components/RecaptchaProvider";
+import Script from "next/script";
 
 const montserratSans = Montserrat({
 	variable: "--font-montserrat-sans",
@@ -101,12 +102,34 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					id="gtm-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K9SWPGBD');
+            `,
+					}}
+				/>
+			</head>
 			<body
 				className={`${montserratSans.variable} ${soraSans.variable} antialiased`}
 			>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-K9SWPGBD"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					></iframe>
+				</noscript>
 				<RecaptchaProvider>
 					<Navbar />
 					{children}
